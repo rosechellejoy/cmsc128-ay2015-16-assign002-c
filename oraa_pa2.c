3838686bc1371e3d2eb31198fc1a9f51207ebd5d
rosechellejoy <rosechellejoyoraa@gmail.com>
2016-03-03 22:57:54 +0800

    Added isValidString()

--- oraa_pa2.c
+++ oraa_pa2.c
@@ -2,7 +2,7 @@
 #include<string.h>
 
 int getHammingDistance(char str1[50], char str2[50]){
-	int len1, len2, dist, i;
+	int dist, i;
 
 	
 	if(strlen(str1)!=strlen(str2)){
@@ -13,42 +13,49 @@ int getHammingDistance(char str1[50], char str2[50]){
 		printf("Error! Empty string(s)\n");
 		return -1;
 	}
-	for(i=0; i<strlen(str1); i++){
+
+	for(i=0; i<strlen(str1); i++) 
 		if(str1[i]!=str2[i]) dist+=1;
-	}
 
 	return dist;
 
 }
 
-int main(){
-	int ch, res;
-	char str1[50], str2[50];
-	printf("------Menu------\n");
-	printf("[1] getHammingDistance\n");
-	printf("[2] countSubstrPattern\n");
-	printf("[3] isValidString\n");
-	printf("[4] getSkew\n");
-	printf("[5] getMaxSkewN\n");
-	printf("[6] getMinSkewN\n");
-	printf("[0] Exit\n");
-	printf("Choice: ");
-	scanf("%d", &ch);
+int countSubstrPattern(char original[50], char pattern[50] ){
 
-	switch(ch){
-		case 1:	printf("str1: ");	
-				scanf( "%s",str1);
-				printf("str2: ");	
-				scanf( "%s",str2);
-				res=getHammingDistance(str1, str2);
-				if(res>0) printf("%d", res);
-				break;
-		case 2:
-		case 3:
-		case 4:
-		case 5:
-		case 6:
-		case 7: printf("Bye!\n");
-				break;
+}
+
+int isValidString(char str[50], char alphabet[50]){
+	int i, j;
+
+	for(i=0; i<strlen(str); i++){
+		for(j=0; j<strlen(alphabet); j++)
+			if(str[i]==alphabet[j]) break; 
+		if(j==strlen(alphabet)) return 1;
 	}
+	return 0;
+}
+
+int getSkew(char str[50], int n){
+	
+}
+
+int getMaxSkewN(char str[50], int n){
+	
+}
+
+int getMinSkewN(char str[50], int n){
+	
+}
+
+int main(){
+	int res;
+	char str1[50], str2[50];
+	printf("str1: ");	
+	scanf( "%s",str1);
+	printf("str2: ");	
+	scanf( "%s",str2);
+	res=isValidString(str1, str2);
+	printf("%d", res);
+
 }
